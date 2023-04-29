b4c2aa125bfcd4518cd9f584e06a24ddb90cb163
bob <aalbayda@up.edu.ph>
2023-04-29 22:04:51 +0800

    Revert "Apr 29"
    This reverts commit 59216fdd1abf40337b12e9f6fc34cd1cbf1307df.

--- algo++_mocks/mock-apr-29/calculator.py
+++
@@ -1,9 +0,0 @@
-# https://open.kattis.com/problems/calculator
-
-while True:
-    try:
-        expr = input()
-        ans = eval(expr)
-        print("{:.2f}".format(ans))
-    except:
-        break
